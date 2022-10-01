92c3f542d0c575f8cf9e9530be14ef67d3074601
tison <wander4096@gmail.com>
2022-10-02 01:14:52 +0800

    feat: RocksDB backend without checkpoint (#17)
    Signed-off-by: tison <wander4096@gmail.com>

--- zeronos-server/src/main/java/io/korandoru/zeronos/server/DataMapStatemachine.java
+++ zeronos-server/src/main/java/io/korandoru/zeronos/server/DataMapStatemachine.java
@@ -20,19 +20,39 @@ import io.korandoru.zeronos.core.proto.GetRequest;
 import io.korandoru.zeronos.core.proto.GetResponse;
 import io.korandoru.zeronos.core.proto.PutRequest;
 import io.korandoru.zeronos.core.proto.PutResponse;
-import java.util.Map;
+import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ratis.protocol.Message;
+import org.apache.ratis.protocol.RaftGroupId;
+import org.apache.ratis.server.RaftServer;
+import org.apache.ratis.server.storage.RaftStorage;
 import org.apache.ratis.statemachine.TransactionContext;
 import org.apache.ratis.statemachine.impl.BaseStateMachine;
 import org.apache.ratis.thirdparty.com.google.protobuf.ByteString;
 import org.apache.ratis.thirdparty.com.google.protobuf.InvalidProtocolBufferException;
+import org.rocksdb.Options;
+import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
 
 @Slf4j
 public class DataMapStatemachine extends BaseStateMachine {
-    private final Map<String, String> dataMap = new ConcurrentHashMap<>();
+    private RocksDB db;
+
+    @Override
+    public void initialize(RaftServer raftServer, RaftGroupId raftGroupId, RaftStorage storage) throws IOException {
+        super.initialize(raftServer, raftGroupId, storage);
+        try (Options options = new Options().setCreateIfMissing(true)) {
+            this.db = RocksDB.open(options, storage.getStorageDir().getTmpDir().getAbsolutePath());
+        } catch (RocksDBException e) {
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    public void close() {
+        this.db.close();
+    }
 
     @Override
     public CompletableFuture<Message> query(Message request) {
@@ -41,22 +61,26 @@ public class DataMapStatemachine extends BaseStateMachine {
         try {
             requestBuilder.mergeFrom(GetRequest.parseFrom(request.getContent()));
         } catch (InvalidProtocolBufferException e) {
-            log.warn("Receiving invalid message: {}", request, e);
-            responseBuilder.setFound(false);
+            log.error("Receiving invalid message: {}", request, e);
+            return CompletableFuture.failedFuture(e);
+        }
+
+        try {
+            final var k = requestBuilder.getKey();
+            final var v = db.get(k.toByteArray());
+            if (v == null) {
+                responseBuilder.setFound(false);
+            } else {
+                responseBuilder.setFound(true);
+                responseBuilder.setKey(k);
+                responseBuilder.setValue(ByteString.copyFrom(v));
+            }
+
             return CompletableFuture.completedFuture(Message.valueOf(responseBuilder.build().toByteString()));
+        } catch (RocksDBException e) {
+            log.error("Cannot read from RocksDB for request: {}", request, e);
+            return CompletableFuture.failedFuture(e);
         }
-
-        final var k = requestBuilder.getKey().toStringUtf8();
-        final var v = dataMap.get(k);
-        if (v == null) {
-            responseBuilder.setFound(false);
-        } else {
-            responseBuilder.setFound(true);
-            responseBuilder.setKey(requestBuilder.getKey());
-            responseBuilder.setValue(ByteString.copyFromUtf8(v));
-        }
-
-        return CompletableFuture.completedFuture(Message.valueOf(responseBuilder.build().toByteString()));
     }
 
     @Override
@@ -65,16 +89,21 @@ public class DataMapStatemachine extends BaseStateMachine {
         final var requestBuilder = PutRequest.newBuilder();
         final var entry = trx.getLogEntry();
         final var request = entry.getStateMachineLogEntry();
+
         try {
             requestBuilder.mergeFrom(request.getLogData());
         } catch (InvalidProtocolBufferException e) {
-            log.warn("Receiving invalid message: {}", request, e);
-            return CompletableFuture.completedFuture(response);
+            log.error("Receiving invalid message: {}", request, e);
+            return CompletableFuture.failedFuture(e);
         }
 
-        final var k = requestBuilder.getKey().toStringUtf8();
-        final var v = requestBuilder.getValue().toStringUtf8();
-        dataMap.put(k, v);
+        final var k = requestBuilder.getKey().toByteArray();
+        final var v = requestBuilder.getValue().toByteArray();
+        try {
+            db.put(k, v);
+        } catch (RocksDBException e) {
+            return CompletableFuture.failedFuture(e);
+        }
 
         // update the last applied term and index
         final long index = entry.getIndex();
