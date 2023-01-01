88aab2127a23e54f0ae8d1f8a1f2fb8f640a8fa4
tison <wander4096@gmail.com>
2023-01-01 17:57:32 +0800

    build: Configure license-maven-plugin
    Signed-off-by: tison <wander4096@gmail.com>

--- zeronos-core/src/main/java/io/korandoru/zeronos/core/config/ServerConfig.java
+++ zeronos-core/src/main/java/io/korandoru/zeronos/core/config/ServerConfig.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.korandoru.zeronos.core.config;
 
 import com.fasterxml.jackson.dataformat.toml.TomlMapper;
--- zeronos-core/src/main/java/io/korandoru/zeronos/core/config/model/RaftPeerModel.java
+++ zeronos-core/src/main/java/io/korandoru/zeronos/core/config/model/RaftPeerModel.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.korandoru.zeronos.core.config.model;
 
 public record RaftPeerModel (String id, String address) {}
